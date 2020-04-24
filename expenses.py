bb0e303310d71b094bd780b3a5ac6df3480e6f16
DimitryLenchevsky <overborsch@gmail.com>
2020-04-24 11:02:20 +0300

    add new task

---
+++ expenses.py
@@ -0,0 +1,7 @@
+food = float(input('Давайте посчитаем сколько денег вы потратили в этом месяце. Сколько вы потратили на еду?'))
+appartment = float(input("Сколько вы потратили на квартиру?"))
+transport = float(input("Сколько вы потратили на транспорт?"))
+fun = float(input("Сколько вы поратили на развлечения?"))
+education = float(input("Сколько вы потратили на образование?"))
+total = food + appartment + transport + fun + education
+print("За месяц вы потратили в итоге", total, "рублей.")
