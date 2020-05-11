3365b6d7a136670c69f2696e99622972fa938d50
DimitryLenchevsky <overborsch@gmail.com>
2020-05-11 16:29:57 +0300

    add

--- test.py
+++ test.py
@@ -1,53 +1,63 @@
-def conclusion_address_book():  # Выводит ключи и значения словаря
-    for key, value in address_book.items():
+def conclusion_person():
+    for key, value in person.items():
         print(key, ",", value)
 
 
-def write_to_file():  # Записывает данные из словаря в файл
-    with open("address_book.txt", 'w', encoding="utf-8") as f:
-        for key, value in address_book.items():
-            f.write("{} : {}\n".format(key, value))
+choice = None
 
-
-choice = None  # Выбор человека
-
-address_book = {}
+person = {}
 
 while choice != 1:
 
-    print("\t1. Exit \n\
-    2. Delete contact \n\
-    3. Add contact \n\
-    4. Replace contact \n")
+    print("\t1. Выход \n\
+    2. Add contact \n\
+    3. Replace contact \n")
 
     choice = int(input("\tYour choice: \n"))
 
     if choice == 1:
         exit()
 
-    elif choice == 2:
+    elif choice == 2 or 3:
         try:
-            conclusion_address_book()
+            conclusion_person()
             print()
-            delete_value_from_dict = input("Who do you want to remove?(Enter name): ")
-            del address_book[delete_value_from_dict]  # Удаляет значения и ключи из словаря
-            conclusion_address_book()
+            key = input("Введите ФИО: ")
+            weight = int(input("Введите массу тела в килограммах: "))
+            height = int(input("Введите Ваш рост в сантиметрах: "))
+            sex = input("Введите Ваш пол: ")
+            age = int(input("Введите Ваш возраст: "))
+            value = [weight, height, sex, age]
+            person[key] = value
+            conclusion_person()
             print()
-            print(address_book)
+            bmi = weight / (height * height / 10000)
+
+            print("Индекс Вашей массы тела равен: ", bmi)
+            if bmi <= 16:                                    
+                print("Выраженный дефицит массы тела, больше кушайте!")
+            elif bmi <= 18.5:
+                print("Недостаточная (дефицит) масса тела. Больше кушайте!")
+            elif bmi <= 25:
+                print("Ваш индекс массы тела в норме")
+            elif bmi <= 30:
+                print("Избыточная масса тела (предожирение), постарайтесь больше двигаться!")
+            elif bmi <= 35:
+                print("Ожирение, постарайтесь больше двигаться и меньше есть!")
+            elif bmi <= 40:
+                print("Ожирение резкое, срочно сходите к врачу!")
+            elif bmi > 40:
+                print("Очень резкое ожирение, срочно сходите к врачу!")
+            pass
+
+            tick = "|"
+            first = bmi - 10
+            second = 50 - bmi
+
+            scale = '10' + "=" * int(first) + tick + "=" * int(second) + "50"
+            print(scale)
         except Exception:
             print("O-o-ops, something went wrong")
 
-    elif choice == 3 or 4:
-        try:
-            conclusion_address_book()
-            print()
-            key = input("Enter name: ")
-            value = input("Enter address: ")
-            address_book[key] = value
-            conclusion_address_book()
-            print()
-            write_to_file()
-        except Exception:
-            print("O-o-ops, something went wrong")
-
-print(address_book)
+print("Полный список пользователей: ")
+print(person)
