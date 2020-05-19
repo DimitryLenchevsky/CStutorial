80e983dec96d740169b5afd28b19273e88da4e44
DimitryLenchevsky <overborsch@gmail.com>
2020-05-20 01:16:23 +0300

    class practice

--- class_test.py
+++ class_test.py
@@ -1,13 +1,17 @@
 class Car:
-    def __init__(self, model):
+    wheels = 4
+    def __init__(self, model, engine_volume):
         self.model = model
+        self.__engine__ = engine_volume
+    def ready(self):
+        print("The {} car with {} engine volume is ready to drive!!!" .format(self.model, self.__engine__))
     def start_engine(self):
         print("Model {} has started the engine, wroom wroom!!!" .format(self.model))
-    def drive(self, speed):
-        self.speed = speed
-        print("Model is driving now {} km/h" .format(self.speed))
+    def drive(self):
+        print("{} is driving now" .format(self.model))
 
-car = Car("Ferari123")
-car.start_engine()
-car.drive("30")
-car.model
+car = Car("Ferari", 5.5)
+print(car.ready())
+print(car.start_engine())
+print(car.drive())
+
