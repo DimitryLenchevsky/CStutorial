1e260698bf5293ed455f6bd9d1293c122d05e212
DimitryLenchevsky <overborsch@gmail.com>
2020-05-26 21:46:52 +0300

    datetime

--- rec.py
+++ rec.py
@@ -1,19 +1,3 @@
-import main
-main.a__b(3, 4)
-# structure = [
-#     1,
-#     2,
-#     '3'
-# ]
-
-# def cnt(st):
-#     accum = 0
-#     for i in st:
-#         if isinstance(i, int):
-#             print(i)
-#         else:
-#             accum += cnt(i)
-#     return accum
-
-# print(cnt(structure))
-
+import datetime
+cdt = datetime.datetime.now()
+print(cdt)
