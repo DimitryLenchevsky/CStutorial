6e445f24f3442bf75237e9ef65f947dc268578fb
DimitryLenchevsky <overborsch@gmail.com>
2020-05-27 17:53:43 +0300

    datetime.datetime

--- rec.py
+++ rec.py
@@ -1,5 +1,6 @@
 import datetime
 import pytz
+import calendar
 
 #timezone
 timezone = pytz.timezone('Europe/Minsk')
@@ -9,6 +10,16 @@ print(timezone)
 tn = datetime.datetime.now()
 print(type(tn))
 
+#.strftime
 str_tn = tn.strftime('%d.%Y.%A %H.%M.%S')
 print(str_tn)
 
+#.strptime
+time_as_atring = "18.10.20 13:27"
+time_obj = datetime.datetime.strptime("1810201327", "%d%m%y%H%M")
+print(time_obj)
+
+#calendar
+wd = calendar.Calendar()
+for d in wd.iterweekdays():
+    print(d)
