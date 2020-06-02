c809f780190ab71b9adda8dbce6c159237f65330
DimitryLenchevsky <overborsch@gmail.com>
2020-06-02 21:19:23 +0300

    class

--- RPS.py
+++ RPS.py
@@ -5,6 +5,11 @@ class Dog:
 
 dog = Dog('Белка', 3)
 dog1 = Dog('Стрелка', 4)
+c = Dog('Полина', 10)
 
-print(dog.name, dog.age, "годика")
-print(dog1.name, dog1.age, "годика")
+#print(dog.name, dog.age, "годика")
+#print(dog1.name, dog1.age, "годика")
+print(c.name, c.age, 'годиков')
+
+dog.i_wasnt_here = 'string'
+print(dog.i_wasnt_here)
