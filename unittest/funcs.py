630856bae4c7f06e940bf2a8e5f2d0e682de068b
DimitryLenchevsky <overborsch@gmail.com>
2020-08-28 18:31:43 +0300

    unittests

---
+++ unittest/funcs.py
@@ -0,0 +1,17 @@
+'''
+Module consists of functions to test 
+'''
+import requests
+from requests.exceptions import ConnectionError
+from typing import List, Union
+
+def my_mean(in_list: List[Union[int, float]]) -> float:
+    """
+    Calculates mean of the provided list
+    """
+    summ = 0
+    count = 0
+    for i in in_list:
+        count += 1
+        summ += i
+    return summ / count
---
+++ unittest/tests.py
@@ -0,0 +1,21 @@
+import unittest
+from funcs import my_mean
+
+class TestMyMean(unittest.TestCase):
+
+    def test_my_mean_one_int(self):
+        self.assertEqual(my_mean([1,]), 1)
+
+    def test_my_mean_many_ints(self):
+        self.assertEqual(my_mean([1, 2, 3]), 2)
+
+    def test_my_mean_many_floats(self):
+        self.assertEqual(my_mean([1.1, 2.2, 3.3]), 6.6/3)
+
+    def test_my_mean_empty_list(self):
+        with self.assertRaises(ZeroDivisionError):
+            res = my_mean([])
+
+
+if __name__ == '__main__':
+    unittest.main()
