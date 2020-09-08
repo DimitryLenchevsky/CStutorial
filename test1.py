d41a91ba1388c0502fa5a18d7d720e88d45646b6
DimitryLenchevsky <overborsch@gmail.com>
2020-09-08 19:45:32 +0300

    OOP

--- test1.py
+++ test1.py
@@ -1,5 +1,16 @@
 class Dog:
-    def __init__(self):
-        print('I am a dog. Woof woof')
+    paws = 4
+    def __init__(self, a, b):
+        self.name = a
+        self.age = b
+    def bark(self, times):
+        print(f'The dog {self.name} at the age of {self.age} can bark {times} times')
 
-Dog()
+    def run(self):
+        print(f'The {self.name} runs!')
+        dog.bark(3)
+        
+
+dog = Dog('Шарик', 3)
+print(dog.age, dog.name)
+print(dog.run())
