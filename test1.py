c173c1018b1db6c49230088ac6e05406d261f2a5
DimitryLenchevsky <overborsch@gmail.com>
2020-09-09 09:30:43 +0300

    datetime

--- test1.py
+++ test1.py
@@ -1,31 +1,11 @@
-class Dog:
-    paws = 4
-    def __init__(self, a, b):
-        self.name = a
-        self.age = b
-    def bark(self, times):
-        print(f'The dog {self.name} at the age of {self.age} can bark {times} times')
+import datetime
 
-    def run(self):
-        print(f'The {self.name} runs!')
-        dog.bark(3)
-        
+class Interval:
+    end = datetime.datetime.now()
+    def __init__(self, start):
+        self.start = start
+    def get_len(self):
+        return self.end - self.start
 
-dog = Dog('Шарик', 3)
-print(dog.age, dog.name)
-print(dog.run())
-
-
-class Tank:
-    size = "big"
-    def __init__(self, a, b):
-        self.name = a
-        self.power = b
-    def shoot(self):
-        print(f'The {self.name} tank shoots')
-        tank.reloading()
-    def reloading(self):
-        print(f'The {self.name} is realoding its {self.power} ammo')
-
-tank = Tank('t-34', 'powerfull')
-print(tank.shoot())
+my_int = Interval(datetime.datetime(2018,12,12, 19,0,0))
+print(my_int.get_len())
