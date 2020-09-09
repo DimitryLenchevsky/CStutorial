6129c2ec33a7edf20042d142c6bfdf421116a5fe
DimitryLenchevsky <overborsch@gmail.com>
2020-09-09 11:46:10 +0300

    property

--- test1.py
+++ test1.py
@@ -1,11 +1,13 @@
 class Dog:
     paws = 4
-    def __init__(self, name, age, address):
-        self.name = name
-        self.age = age
-        self.__address = address
-    def sed_dog(self):
-        print('Сообщение было отправлено по адресу {}'.format(self.__address))
+    def __init__(self, a, b):
+        self.name = a
+        self.age = b
+    @property
+    def month(self):
+        return self.age * 12
 
-dog = Dog('Шарик', 26, 'Лазо 16')
-print(dog.sed_dog())
+dog = Dog('Шарик', 3)
+print(dog.month())
+dog.month = 45
+print(dog.month())
