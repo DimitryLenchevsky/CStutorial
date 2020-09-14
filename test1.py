4ca4534263695d85d9f86b56b91df4b7e06a948b
DimitryLenchevsky <overborsch@gmail.com>
2020-09-14 15:59:41 +0300

    practice

--- test1.py
+++ test1.py
@@ -1,11 +1,16 @@
-class Dog:
-    paws = 4
-    def __init__(self, a, b):
-        self.name = a
-        self.age = b
-    @property
-    def month(self):
-        return self.age * 12
+import random
 
-dog = Dog('Шарик', 3)
-print(dog.month())
+def pass_gen(min_length=5):
+    a = ['a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4']
+    password_list = []
+    random.shuffle(a)
+    password_list = ''.join([random.choice(a) for x in range(min_length)])
+    return password_list
+
+pwd = pass_gen(min_length=5)
+print(pwd)
+
+dict = {}
+dict[1] = pwd
+
+print(dict)
