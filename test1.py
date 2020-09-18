c6b3f11e7e4094f3addf9b843ff096370f9e74a7
DimitryLenchevsky <overborsch@gmail.com>
2020-09-18 14:34:09 +0300

    decorator test

--- test1.py
+++ test1.py
@@ -1,17 +1,14 @@
-from polymorphism import Rectangle, Square, Circle
+def decorator(func):
+    def wrapper():
+        print('Код до выполнения функции')
+        func()
+        print('Код который сработал после функции')
+    return wrapper
 
-triangle1 = Rectangle(4, 6)
-triangle2 = Rectangle(5, 8)
-print(triangle1.get_area(), triangle2.get_area())
+@decorator
+def show():
+    print('Я обычная функция')
 
-square1 = Square(4)
-square2 = Square(6)
-print(square1.get_area(), square2.get_area())
 
-circ1 = Circle(3)
-circ2 = Circle(2)
-print(circ1.get_area(), circ2.get_area())
-
-figures = [triangle1, triangle2, square1, square2, circ1, circ2]
-for figure in figures:
-        print(figure.get_area())
+#show = decorator(show)
+show()
