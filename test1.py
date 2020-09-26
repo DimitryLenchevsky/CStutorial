ae3d81bc49724fd62c742f749ea4185a02a2ce41
DimitryLenchevsky <overborsch@gmail.com>
2020-09-26 17:54:39 +0300

    add

--- test1.py
+++ test1.py
@@ -9,4 +9,12 @@ while True:
     if answer == 'stop':
         break
 
-print(phone_book)
+
+
+# sort - это метод списка
+# sorted - это встроенная функция
+a = [-4, 5, 300, 136, -469, 235, 2]
+b = 'hello world'
+c = ('albatros', 'trosablat', 'salbatros', 'solbatras')
+c = sorted(c, reverse=True)
+print(c) 
