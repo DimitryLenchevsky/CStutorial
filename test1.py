7878b612c1a016f2e303fc45e04510ad50a58685
DimitryLenchevsky <overborsch@gmail.com>
2020-09-30 22:01:12 +0300

    current practice

--- test1.py
+++ test1.py
@@ -36,3 +36,17 @@ sheet[('A', 1)] = 100
 sheet[('B', 1)] = 1000
 print(sheet[('A', 1)])
 print(sheet)
+
+#dict
+
+phone_book = {}
+while True:
+    name = input('Введите пожалуйста имя пользователя ... ')
+    phone_number = int(input('Введите пожалуйста номер телефона '))
+    phone_book[name] = phone_number
+    print('Запись в телефонную книгу добавлена')
+    answer = input('Если вы желаете больше не добавлять номера - введите stop, если еще введем пользователя - нажмите enter')
+    if answer == 'stop':
+        break
+
+print(phone_book)
