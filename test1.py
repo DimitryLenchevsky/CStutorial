15a3abd361ab482410c7663b8a55b07c8e876437
DimitryLenchevsky <overborsch@gmail.com>
2020-10-02 21:05:56 +0300

    practice

--- test1.py
+++ test1.py
@@ -50,3 +50,13 @@ while True:
         break
 
 print(phone_book)
+
+def pass_gen(min_length=5):
+    a = ['a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4']
+    password_list = []
+    random.shuffle(a)
+    password_list = ''.join([random.choice(a) for x in range(min_length)])
+    return password_list
+
+pwd = pass_gen(min_length=5)
+print(pwd)
