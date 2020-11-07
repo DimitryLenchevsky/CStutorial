ec52ec6864fced7dbdd138b3b0426d3335324334
DimitryLenchevsky <overborsch@gmail.com>
2020-11-07 23:10:20 +0300

    dict

--- test1.py
+++ test1.py
@@ -1,24 +1,6 @@
-class Car:
-    wheels = 4
-    def __init__(self, a, b):
-        self.color = a
-        self.engine_volume = b
-    def start_engine(self):
-        return 'Wroom'
+kwargs = {}
 
-
-class Truck(Car):
-    def open_trunk(self):
-        return 'the trunk is opened'
-    def __secret__(self):
-        return 'secret'
-
-
-
-
-man = Truck('red', 4.5)
-
-print(man.open_trunk())
-print(man.color)
-print(man.__secret__())
-
+name = 'Вася'
+number = 4
+kwargs[name] = number
+print(kwargs)
