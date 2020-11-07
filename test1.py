add8dc099e653979ba44108912392727b62e007c
DimitryLenchevsky <overborsch@gmail.com>
2020-11-07 15:23:26 +0300

    OOP

--- test1.py
+++ test1.py
@@ -1,2 +1,24 @@
-import os
-print('os.name= ', os.name)
+class Car:
+    wheels = 4
+    def __init__(self, a, b):
+        self.color = a
+        self.engine_volume = b
+    def start_engine(self):
+        return 'Wroom'
+
+
+class Truck(Car):
+    def open_trunk(self):
+        return 'the trunk is opened'
+    def __secret__(self):
+        return 'secret'
+
+
+
+
+man = Truck('red', 4.5)
+
+print(man.open_trunk())
+print(man.color)
+print(man.__secret__())
+
