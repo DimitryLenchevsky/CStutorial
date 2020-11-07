1490ffdad48ccc6f6bc1359f401f0aeab2df913d
DimitryLenchevsky <overborsch@gmail.com>
2020-11-07 23:32:52 +0300

    split and join

--- test1.py
+++ test1.py
@@ -1,12 +1,11 @@
-import random
+a = 'fdgf fdgdf s d s gfhfg er'
+b = a.split(' ')
+print(b)
 
 
-def pass_gen(min_length=5):
-    a = ['a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4']
-    password_list = []
-    random.shuffle(a)
-    password_list = ''.join([random.choice(a) for x in range(min_length)])
-    return password_list
+b.sort()
+print(b)
 
-pwd = pass_gen(min_length=5)
-print(pwd) 
+
+c = ' '.join(b)
+print(c)
