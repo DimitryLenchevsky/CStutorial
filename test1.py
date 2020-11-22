897aeb37189efc09caba4d3369826a0b37f61b0e
DimitryLenchevsky <overborsch@gmail.com>
2020-11-22 12:28:14 +0300

    dict

--- test1.py
+++ test1.py
@@ -1,5 +1,12 @@
+name_age = {}
 
-text = 'Рассматривается способ перебора элементов коллекций\n с помощью итераторов. Формирование значений через выражения-генераторы\n и функции-генераторы.Оператор yield. Функции iter и next.'
-print(text)
-z = text.split()
-print(z)
+for i in range(3):
+    name = input('Name? ')
+    age = int(input('Age? '))
+    name_age[name] = age
+
+name_choice = input('Name to find? ')
+if name_choice in name_age:
+    print(name_age[name_choice])
+else:
+    print('There is no such name in the dict')
