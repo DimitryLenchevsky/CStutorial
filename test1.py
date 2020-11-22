b1e42349d1b70fe6d61a117f6d4cc6e2781ba46f
DimitryLenchevsky <overborsch@gmail.com>
2020-11-22 13:26:42 +0300

    подсчет слов в строке

--- test1.py
+++ test1.py
@@ -1,18 +1,7 @@
-name_age = {}
-
-for i in range(3):
-    name = input('Name? ')
-    age = int(input('Age? '))
-    name_age[name] = age
-
-name_choice = input('Name to find? ')
-if name_choice in name_age:
-    print(name_age[name_choice])
-else:
-    print('There is no such name in the dict')
-
-print(dir(name_age))
-print(len(name_age.keys()))
-print(len(name_age.values()))
-print(name_age.__class__())
-print(name_age.__dir__())
+sample_string = 'To be or not to be'
+occurences = {}
+for word in sample_string.split():
+    occurences[word] = occurences.get(word, 0) + 1
+for word in occurences:
+    print('The word', word, 'occurs', occurences[word], \
+        'times in the string')
