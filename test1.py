e111122e15ea472b22f28cdc9bc13b4857e495cd
DimitryLenchevsky <overborsch@gmail.com>
2020-11-24 18:22:07 +0300

    practice

--- test1.py
+++ test1.py
@@ -15,4 +15,28 @@ for x, y in somelist:
 x = [0, 2, 5, -2, -6, 3, 6, -1, 8]
 for i, n in enumerate(x):
     if n < 0:
-        print('Найдено отрицательное число ао индексом ', i)
+        print('Найдено отрицательное число под индексом ', i)
+
+
+x = [1, 2, 3, 4]
+new_x = [i*i for i in x]
+print(new_x)
+
+
+x = [1, 2, 3, 4]
+new_x_dict = {i : i*i for i in x}
+print(new_x_dict)
+
+
+x = [0, 2, 5, -2, -6, 3, 6, -1, 8]
+non_negative_list = [i for i in x if i >= 0]
+print(non_negative_list)
+
+
+aaaaa = (i for i in range(1, 101) if i%2)
+for square in aaaaa:
+    print(square, )
+
+
+cubic = {i : i**3 for i in range(11, 16)}
+print(cubic)
