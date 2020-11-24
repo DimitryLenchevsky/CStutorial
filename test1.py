5a87c45b6528d87b6f187ec261e71a549c577515
DimitryLenchevsky <overborsch@gmail.com>
2020-11-24 16:32:24 +0300

    enumerate

--- test1.py
+++ test1.py
@@ -1,7 +1,18 @@
-sample_string = 'To be or not to be'
-occurences = {}
-for word in sample_string.split():
-    occurences[word] = occurences.get(word, 0) + 1
-for word in occurences:
-    print('The word', word, 'occurs', occurences[word], \
-        'times in the string')
+somelist = [(2, 3), (5, 8), (1, 9)]
+result = 0
+for i in somelist:
+    result = result = (i[0] + i[1])
+    print(result)
+
+
+somelist = [(2, 3), (5, 8), (1, 9)]
+result = 0
+for x, y in somelist:
+    result = result + (x + y)
+    print(result)
+
+
+x = [0, 2, 5, -2, -6, 3, 6, -1, 8]
+for i, n in enumerate(x):
+    if n < 0:
+        print('Найдено отрицательное число ао индексом ', i)
