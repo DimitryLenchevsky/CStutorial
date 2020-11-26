b32d9e6f0b82fb08af84e2acb6b2595aaa461dba
DimitryLenchevsky <overborsch@gmail.com>
2020-11-26 15:18:47 +0300

    func

--- test1.py
+++ test1.py
@@ -1,42 +1,27 @@
-somelist = [(2, 3), (5, 8), (1, 9)]
-result = 0
-for i in somelist:
-    result = result = (i[0] + i[1])
-    print(result)
+def power(x, y=2):
+    r = 1
+    while y > 0:
+        r = r * x
+        y = y - 1
+    return r
+
+first = power(3)
+print(first)
+
+second = power(3, 3)
+print(second)
 
 
-somelist = [(2, 3), (5, 8), (1, 9)]
-result = 0
-for x, y in somelist:
-    result = result + (x + y)
-    print(result)
+def maxinum(*numbers):
+    if len(numbers) == 0:
+        return None
+    else:
+        maxnum = numbers[0]
+        for n in numbers[1:]:
+            if n > maxnum:
+                maxnum = n
+        return maxnum
 
 
-x = [0, 2, 5, -2, -6, 3, 6, -1, 8]
-for i, n in enumerate(x):
-    if n < 0:
-        print('Найдено отрицательное число под индексом ', i)
-
-
-x = [1, 2, 3, 4]
-new_x = [i*i for i in x]
-print(new_x)
-
-
-x = [1, 2, 3, 4]
-new_x_dict = {i : i*i for i in x}
-print(new_x_dict)
-
-
-x = [0, 2, 5, -2, -6, 3, 6, -1, 8]
-non_negative_list = [i for i in x if i >= 0]
-print(non_negative_list)
-
-
-aaaaa = (i for i in range(1, 101) if i%2)
-for square in aaaaa:
-    print(square, )
-
-
-cubic = {i : i**3 for i in range(11, 16)}
-print(cubic)
+result = maxinum(2, 5, 7, -1, 9, 4)
+print(result)
