db6d4150048749ff3be75e59b2d050b0321703fc
DimitryLenchevsky <overborsch@gmail.com>
2020-11-26 17:04:52 +0300

    reversed()

--- test1.py
+++ test1.py
@@ -25,3 +25,22 @@ def maxinum(*numbers):
 
 result = maxinum(2, 5, 7, -1, 9, 4)
 print(result)
+
+
+def example_fun(x, y, **other):
+    print('x: {0}, y: {1}, keys in "other": {2}'.format(x, y, list(other.keys())))
+    other_total = 0
+    for k in other.keys():
+        other_total = other_total + other[k]
+    print('The total of values in "other" is {0}'.format(other_total))
+
+
+result1 = example_fun(2, y='1', foo=3, bar=4)
+print(result1)
+
+
+def reverse(*args):
+    for i in reversed(args):
+        print(i)
+
+b = reverse(3, 5, 6, 7, 1, 9, 6, 2)
