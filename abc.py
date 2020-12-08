f0d9920c12b2be2db3a686f64a9b70c2f9b84c25
DimitryLenchevsky <overborsch@gmail.com>
2020-12-08 12:55:52 +0300

    .count()

---
+++ abc.py
@@ -0,0 +1,5 @@
+animals = ['dog', 'cat', 'frog', 'elephant', 'dog', 'dog', 'cat']
+print("We have ", animals.count('dog'), " dogs in list")
+print("We have ", animals.count('cat'), " cats in list")
+print("We have ", animals.count('frog'), " frog in list")
+print("We have ", animals.count('elephant'), " elephant in list")
--- moonweight.py
+++
@@ -1,3 +0,0 @@
-weight = float(input("Давайье узнаем ваш вес на луне. Введите пожалуйста ваш вес:"))
-moonweight = weight / 6
-print("Ваш вес на луне будет равен", moonweight, "кг.")
