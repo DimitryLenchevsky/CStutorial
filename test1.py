d3572908969377618559c1100342bd36b2a6904c
DimitryLenchevsky <overborsch@gmail.com>
2020-12-11 14:42:44 +0300

    game

--- test1.py
+++ test1.py
@@ -1,40 +1,41 @@
-import unittest
-from expenses import my_mean, site_checker
-from unittest import mock
-
-
-class TestMyMean(unittest.TestCase):
-
-    def test_my_mean_int(self):
-        self.assertEqual(my_mean([1,]), 1)
-
-    def test_my_mean_list(self):
-        self.assertEqual(my_mean([2, 3, 4]), 3)
-
-    def test_my_mean_float(self):
-        self.assertEqual(my_mean([1.1, 2.2, 3.3]), 6.6/3)
-
-    def test_my_mean_empty_list(self):
-        with self.assertRaises(ZeroDivisionError):
-            res = my_mean([])
-
-    def test_my_mean_string(self):
-        with self.assertRaises(TypeError):
-            res = my_mean(['1', 2, 3])
-
-
-class TestSiteChecker(unittest.TestCase):
-    @mock.patch('expenses.get_site_status', return_value=200)
-    def test_tut_by(self, mock_get_site_status):
-        url = 'https://tut.by'
-        self.assertEqual(site_checker(url), f'site { url } is ok.')
-
-    @mock.patch('expenses.get_site_status', return_value='Error')
-    def test_tut_byq(self, mock_get_site_status):
-        url = 'https://tut.byq'
-        self.assertEqual(site_checker(url), f'site { url } is not ok.')
-        self.assertIsNone(mock_get_site_status.assert_called_once_with(url)) # try to uncomment line 30 of funcs.py
-
-
-if __name__ == '__main__':
-    unittest.main()
+import random
+while True:
+  print ("Игра \"Русская рулетка\"")
+  print ("Всего 5 попыток, играете вы и компьютер")
+  raund = 0
+  ver = 0
+  while True:
+    ver = ver + 1
+    u1 = random.randint (ver,6)
+    c1 = random.randint (ver,6)
+    s1 = random.randint (ver,6)
+    raund = raund + 1
+    if raund > 5:
+      print ("Игра окончена, ничья")
+      break;
+    print (str(raund) + " Раунд")
+    kk = int(input("Для выстрела нажмите 1:"))
+    if kk != 1:
+      print ("Ошибка")
+      break;
+    elif kk == 1:
+      if u1 == s1 and c1 != u1:
+        print ("У вас патрон, у компьютера нет патрона, вы проиграли, игра окончена")
+        break ;
+      elif c1 == s1 and u1 != s1:
+        print ("У комьютера патрон, у вас нет патрона, вы выиграли, игра окончена")
+        break ;
+      elif c1 != s1 and u1 != s1:
+        print ("Ничья, у вас у обоих нет патрона")
+      elif c1 == s1 and u1 == s1:
+        print ("Ничья, у вас у обоих по патрону, игра окончена")
+        break ;
+  q = int(input("Хотите ли вы играть снова? Нажмите 1 если да, 2 если нет:"))
+  if q == 1:
+    print ("Перезапускаем игру... \n")
+  elif q == 2:
+    print ("Спасибо за игру!")
+    break ;
+  else:
+    print ("Ошибка, неверный формат")
+    break ;
